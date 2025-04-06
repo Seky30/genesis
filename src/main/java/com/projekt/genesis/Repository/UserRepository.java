ddbab02f142936681630719cf76d26fd03b540f0
Seky30 <martinsekerka1994@gmail.com>
2025-04-06 16:40:15 +0200

    Byl vytvořen projekt genesis.
    Byly vytvořeny požadavky na API :
    
    1.založení nového uživatele
    2.informace o uživateli
    3.informace o všech uživatelích
    4.Upravit informace o uživateli
    5.smazat uživatele
    
    Bylo vytvořeno automatické generovaní UUID a zároveň i person ID je jedinečné - nelze přiřadit stejné personID více uživatelům.
    
    Export sql i z postmana je ve složce resources

--- src/main/java/com/projekt/genesis/Repository/UserRepository.java
+++ src/main/java/com/projekt/genesis/Repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.projekt.genesis.Repository;
 
 import com.projekt.genesis.model.User;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
